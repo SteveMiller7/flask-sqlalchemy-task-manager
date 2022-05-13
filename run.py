1435b50cbb1e14c03a981c77f30c31c59c3630fc
SteveMiller7 <info@millercreativedesign.com>
2022-05-13 18:18:55 +0000

    commit

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-
-   
 import os
 from taskmanager import app
 
@@ -10,3 +8,4 @@ if __name__ == "__main__":
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
+    
